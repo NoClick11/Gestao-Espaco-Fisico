133cfd33469722abe9676bd3cf9721a48e5a0890
NoClick11 <bahamonde.boullosa@hotmail.com>
2025-05-31 14:42:11 -0300

    Project Creaetion

---
+++ src/main/java/com/manuelneto/gestaoespacofisico/GestaoEspacoFisicoApplication.java
@@ -0,0 +1,13 @@
+package com.manuelneto.gestaoespacofisico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEspacoFisicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEspacoFisicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuelneto/gestaoespacofisico/GestaoEspacoFisicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuelneto.gestaoespacofisico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEspacoFisicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
